87d36e7abd64f3565cadf7441c6515964025097d
Izzat <30817152+shishhhh@users.noreply.github.com>
2020-12-24 16:53:49 +0800

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+<div class="fixed-bottom p-5 bg-dark text-white" >
+  <p class="text-center">the very first project</p>
+</div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+  include_once 'include/header.php';
+?>
+
+  <div class="container p-5 align-items-center bg-light" >
+    <figure class="text-center">
+      <blockquote class="blockquote">
+        <samp><?php print 'Hello, World'; ?> </samp>
+      </blockquote>
+      <figcaption class="blockquote-footer">
+        Anonymous
+      </figcaption>
+    </figure>
+  </div>    
+
+  <div class="container text-center" style="margin-top:30px">
+    <div class="row">
+      <p>
+        The very first project generally outputs or displays <kbd>'Hello, World'</kbd> using PHP
+      </p>  
+    <div class="row">
+      <p>
+        Programming Language: PHP <br>
+        Tools: Sublime Text & XAMPP <br>
+        Reference:  <a href="https://getbootstrap.com/docs/5.0/getting-started/introduction/">Bootstrap</a>
+      </p>        
+      <br>
+    </div>
+  </div>
+
+<?php
+  include_once 'include/footer.php';
+?>
